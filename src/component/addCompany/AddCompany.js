c7686a94c60a75161dec54b0d799cdf2772138ff
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-09 03:39:15 +0300

    Update AddCompany.js

--- src/component/addCompany/AddCompany.js
+++ src/component/addCompany/AddCompany.js
@@ -6,8 +6,7 @@ import { CountryData } from "../../utils/mockup";
 import { getResponse } from '../../services/axios';
 import { useNavigate } from "react-router-dom";
 
-import 'react-phone-number-input/style.css';
-import "./AddCompany.css";
+
 
 const AddCompany = () => {
     const navigate = useNavigate();
