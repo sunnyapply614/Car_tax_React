c436c09046ca1363351694cc9fa771afbc1e101d
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-13 08:55:15 +0300

    Update Devices.js

--- src/component/devices/Devices.js
+++ src/component/devices/Devices.js
@@ -1,4 +1,10 @@
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from "@mui/material";
+import { CSVLink } from "react-csv";
+import { faMicrochip } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { removeDevice } from "../../services/axios";
 import { Ruptelas, Teltonikas } from "../../utils/mockup";
