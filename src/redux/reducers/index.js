b47406cbb9f3f12b344df55c4575986656439835
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-14 04:21:28 +0300

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,3 +4,8 @@ import deviceReducer from "./deviceReducer";
 import authReducer from "./authReducer";
 import globalReducer from "./globalReducer";
 
+export default combineReducers({
+    devicesList: deviceReducer,
+    auth: authReducer,
+    global: globalReducer,
+});
