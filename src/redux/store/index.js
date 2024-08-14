a1517d7a47600711dfe76b4aeafb9cc5c8df894a
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-14 04:22:04 +0300

    Update index.js

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import storage from 'redux-persist/lib/storage'
-import { persistReducer } from 'redux-persist'
+
 
 import rootReducer from "../reducers";
 
