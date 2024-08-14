52de8031f7cfb79710cbd5acdcc51b47ecb2a251
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-14 04:19:22 +0300

    Update deviceReducer.js

--- src/redux/reducers/deviceReducer.js
+++ src/redux/reducers/deviceReducer.js
@@ -1,4 +1,12 @@
+import { GET_DEVICES, DEVICES_ERROR, GET_USERTOKEN, GET_TOKENERROR, GET_USERS, GET_USERNAMELIST } from "../store/types";
 
+const initialState = {
+    users: [],
+    devices: [],
+    token: "",
+    loading: true,
+    userList: []
+};
 
 export default function (state = initialState, action) {
     switch (action.type) {
