06f860c2e4e18f7509ad1ba7fc9aef70070a4ce4
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-16 04:59:41 +0300

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,1 @@
-// Reducer/index
-import { combineReducers } from "redux";
-import deviceReducer from "./deviceReducer";
-import authReducer from "./authReducer";
-import globalReducer from "./globalReducer";
 
-export default combineReducers({
-    devicesList: deviceReducer,
-    auth: authReducer,
-    global: globalReducer,
-});
