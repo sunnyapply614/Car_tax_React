d0660da384c3fe8b9adade65996a1fff7cd022c3
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-29 02:42:46 +0300

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -15,14 +15,7 @@ const persistConfig = {
     storage,
 }
 
-export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-        serializableCheck: {
-            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-        }
-    }),
-});
+
 export let persistor = persistStore(store);
 
 export const useAppDispatch = () => useDispatch();
