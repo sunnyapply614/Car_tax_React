6c2e1da459f3f8ab6fd9c5029202f3029baf0679
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-28 04:04:32 +0300

    Update devices.js

--- src/redux/actions/devices.js
+++ src/redux/actions/devices.js
@@ -28,5 +28,17 @@ export const getDevices = (props) => async (dispatch) => {
     });
   }
 };
-
+export const getToken = (props) => async (dispatch) => {
+  try {
+    dispatch({
+      type: GET_USERTOKEN,
+      payload: await signInUser(props),
+    });
+  } catch (error) {
+    dispatch({
+      type: GET_TOKENERROR,
+      payload: error,
+    });
+  }
+};
 
