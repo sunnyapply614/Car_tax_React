80c25f2a0e7aacfefec9d50ce52f947c85e28b5e
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-30 08:07:36 +0300

    Update axios.js

--- src/services/axios.js
+++ src/services/axios.js
@@ -138,18 +138,7 @@ const getUser = async (props) => {
   }
 };
 
-const getGoogleAPIKey = async (props) => {
-  try {
-    var result = await axios.post("/api/users/getGoogleAPIKey", props, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return result.data;
-  } catch (error) {
-    return error.response;
-  }
-};
+
 
 const removeDevice = async (props) => {
   try {
