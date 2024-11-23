b89f90588a72873c486647b648e5ce35063c4fda
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-23 00:08:26 -0800

    Update GoogleMap.js

--- src/component/dashboard/GoogleMap.js
+++ src/component/dashboard/GoogleMap.js
@@ -22,7 +22,10 @@ const GoogleMapComponent = ({devices, point}) => {
     lng: 2.1734
   })
 
-
+  const mapStyles = {
+    width: '100%',
+    height: '100%',
+  };
 
   const onLoad = React.useCallback(function callback(map) {
     mapRef.current = map;
