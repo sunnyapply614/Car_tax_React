08ef07b6834f8effaf0856fe4e2883827a19a85c
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-11-27 00:32:27 -0800

    Update AddVehicle.js

--- src/component/Rule-addVehicle/AddVehicle.js
+++ src/component/Rule-addVehicle/AddVehicle.js
@@ -1,8 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useMediaQuery } from "@mui/material";
-import checkbox from "rc-checkbox";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 import { Teltonikas, Ruptelas } from "../../utils/mockup";
 
